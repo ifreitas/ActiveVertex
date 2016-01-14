37a30bd9c9be49f06aca1cbbeaaeb8dfaa9eddbf
ifreitas <israel.araujo.freitas@gmail.com>
2016-01-14 02:34:12 -0300

    More time to Timeout

--- src/test/scala/models/BaseSpec.scala
+++ src/test/scala/models/BaseSpec.scala
@@ -5,5 +5,5 @@ import org.scalatest.time.SpanSugar
 
 // I do not use *ParallelTestExecution* because it is breaking the order of the spec output.
 abstract class BaseSpec extends WordSpec with MustMatchers with ScalaFutures with SpanSugar with BeforeAndAfterAll{
-	implicit val defaultPatience = PatienceConfig(timeout = 3 seconds)
+	implicit val defaultPatience = PatienceConfig(timeout = 10 seconds)
 }
